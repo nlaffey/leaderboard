9297e20838baabf3211fcd289f444e0c56c91b30
Nick Laffey <nick.laffey@gmail.com>
2016-09-23 23:15:11 -0400

    Fixing unique name bug

--- src/models/Player.js
+++ src/models/Player.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 var schema = mongoose.Schema({
-    name: {type: String, index: {unique: true, dropDups: true}},
-    id: mongoose.Schema.Types.ObjectId,
+    name: {type: String, index: true, unique: true},
     win: {type: Number, default: 0},
     loss: {type: Number, default: 0},
     ratio: {type: Number, default: 0},

8b97c53ee8ced039aedd4fe9ed2343559015140d
Nick Laffey <nick.laffey@gmail.com>
2016-09-24 01:06:19 -0400

    adding heroku credentials

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
-
 const dbConnection = mongoose.connection;
-const dbAddress = 'mongodb://localhost/leaderboard';
+const isDeveloping = process.env.NODE_ENV !== 'production';
+
+var dbAddress;
+
+if (isDeveloping) {
+    dbAddress = 'mongodb://localhost/leaderboard';
+} else {
+    dbAddress = "mongodb://nlaffey:leaderboard@ds025583.mlab.com:25583/heroku_9g0m2zq4";
+}
 mongoose.connect(dbAddress);
 
 dbConnection.on('error', console.error.bind(console, 'connection error:'));
